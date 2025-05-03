a7a864a49d9ac608381ebf0ec6c937f34e4f6855
PavelBzrkv <pavel95959@gmail.com>
2025-05-03 12:44:27 +0500

    Second commit in another branch

--- Test repository/src/Main.java
+++ Test repository/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I have a dog");
-        System.out.println("Her name is Lisa");
-        System.out.println("She is a very smart dog");
+        System.out.println("some text");
+
     }
 }
