20527438f8a6d2094a276775ee641c1f0218d0d1
PavelBzrkv <pavel95959@gmail.com>
2025-05-03 12:41:16 +0500

    First commit in another branch

--- Test repository/src/Main.java
+++ Test repository/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello everyone");
+        System.out.println("I have a dog");
+        System.out.println("Her name is Lisa");
+        System.out.println("She is a very smart dog");
     }
 }
