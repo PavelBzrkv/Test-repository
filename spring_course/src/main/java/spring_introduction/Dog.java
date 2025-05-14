f6ec233b8fca755d82f6b8864d1f93b22f915b60
PavelBzrkv <pavel95959@gmail.com>
2025-05-14 13:24:03 +0500

    Spring commit two

--- spring_course/src/main/java/spring_introduction/Dog.java
+++ spring_course/src/main/java/spring_introduction/Dog.java
@@ -1,9 +1,25 @@
 package spring_introduction;
 
 public class Dog implements Pet{
+//    private String name;
 
     public Dog() {
         System.out.println("Dog bean is created");
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+    protected void init(){
+        System.out.println("Class Dog: init method");
+    }
+
+    private void destroy(){
+        System.out.println("Class Dog: destroy method");
     }
 
     @Override
