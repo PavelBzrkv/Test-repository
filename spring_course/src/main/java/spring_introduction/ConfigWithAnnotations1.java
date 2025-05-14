7af2a9c44f135924bd625f941c9e4da0cce317b2
PavelBzrkv <pavel95959@gmail.com>
2025-05-14 14:47:47 +0500

    Spring commit Three

--- spring_course/src/main/java/spring_introduction/ConfigWithAnnotations1.java
+++ spring_course/src/main/java/spring_introduction/ConfigWithAnnotations1.java
@@ -2,18 +2,22 @@ package spring_introduction;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test3 {
+public class ConfigWithAnnotations1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext3.xml");
-       // Pet pet = context.getBean("myPet", Pet.class);
-      //  Pet pet = new Cat();
-//        Person person = context.getBean("myPerson", Person.class);
-//        person.callYourPet();
 
-//        System.out.println(person.getSurname());
-//        System.out.println(person.getAge());
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+
+
+
+//        Cat myCat = context.getBean("catBean", Cat.class);
+//        myCat.say();
+
+
 
         context.close();
+
     }
 }
--- spring_course/src/main/java/spring_introduction/Person.java
+++ spring_course/src/main/java/spring_introduction/Person.java
@@ -1,28 +1,30 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
     private int age;
 
 
-    public Person() {
-        System.out.println("Person bean is created");
-    }
-
-//    public Person(Pet pet) {
+//    public Person() {
 //        System.out.println("Person bean is created");
-//        this.pet = pet;
 //    }
+    @Autowired
+    public Person(Pet pet) {
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
 
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
-    public String getSurname() {
-        return surname;
-    }
+
 
     public void setSurname(String surname) {
         System.out.println("Class Person: set surname");
